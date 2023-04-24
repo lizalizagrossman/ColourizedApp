c43c97a993e301d327d6e11843d563c696aacc9f
Liza Grossman <lisok.sam@gmail.com>
2023-04-24 19:33:24 +0300

    Initial Commit

---
+++ ColourizedApp/ColourizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColourizedAppApp.swift
+//  ColourizedApp
+//
+//  Created by Elizabeth on 24/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ColourizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
